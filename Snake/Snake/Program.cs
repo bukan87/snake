78c2da17a080f5e43cab7f1818c927774a3df9f2
unknown <bukan87@mail.ru>
2015-09-29 23:29:36 +0300

    as

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Hello world!");
+=======
+            Console.SetBufferSize(80, 25);
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rigthLine = new VerticalLine(78, 0, 24, '+');
+
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rigthLine.Draw();
+
+            Snake snake = new Snake(new Point(2, 3, '*'), 10, Direction.DOWN);
+            snake.Draw();
+                     
+>>>>>>> Stashed changes
             Console.ReadLine();
         }
     }
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int x, int rTop, int rBotom, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = rTop; y <= rBotom; y++)
+            {
+                pList.Add(new Point(x, y, sym));
+            }
+        }
+    }
+}
