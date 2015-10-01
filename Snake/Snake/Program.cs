53d6e215249b308b2406fb0ef08499ec6e25e06a
unknown <bukan87@mail.ru>
2015-10-01 23:58:30 +0300

    PreFinish

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,17 +11,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
             Console.SetBufferSize(80, 25);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
-            VerticalLine rigthLine = new VerticalLine(78, 0, 24, '+');
 
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rigthLine.Draw();
+            Walls walls = new Walls('+');
+            walls.Draw();
 
             Snake snake = new Snake(new Point(2, 3, '*'), 3, Direction.RIGHT);
             snake.Draw();
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        int width;
+        int height;
+
+        List<Figure> wallList;
+
+        public Walls(char sym)
+        {
+            wallList = new List<Figure>();
+
+            this.width = Console.BufferWidth;
+            this.height = Console.BufferHeight;
+                        
+            HorizontalLine upLine = new HorizontalLine(0, this.width - 2, 0, sym);
+            HorizontalLine downLine = new HorizontalLine(0, this.width - 2, this.height - 1, sym);
+            VerticalLine leftLine = new VerticalLine(0, 0, this.height - 1, sym);
+            VerticalLine rigthLine = new VerticalLine(this.width - 2, 0, this.height - 1, sym);
+
+            wallList.Add(upLine);
+            wallList.Add(downLine);
+            wallList.Add(leftLine);
+            wallList.Add(rigthLine);
+            Draw();
+        }
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
